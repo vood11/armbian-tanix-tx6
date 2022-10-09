039917f8c2dce06911b697e09988b54956e98ca3
dansh <smartlinux@ya.ru>
2022-10-09 08:08:19 +0200

    edit main.sh

--- lib/main.sh
+++ lib/main.sh
@@ -501,11 +501,9 @@ if ! ls "${DEB_STORAGE}/armbian-firmware_${REVISION}_all.deb" 1> /dev/null 2>&1 
 			FULL=""
 			REPLACE="-full"
 			compile_firmware
-			if [[ "${COMPILE_FULL_FW}" == yes ]]; then
-				FULL="-full"
-				REPLACE=""
-				compile_firmware
-			fi
+			FULL="-full"
+			REPLACE=""
+			compile_firmware
 		}
 
 	fi
